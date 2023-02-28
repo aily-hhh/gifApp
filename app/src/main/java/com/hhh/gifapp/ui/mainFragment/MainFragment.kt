89bf5044d808650e5ba59c0a684ba549bcfd17bb
Aily-Hhh <78144867+aily-hhh@users.noreply.github.com>
2023-02-28 14:43:19 +0300

    update
    fragments + navigation

---
+++ app/src/main/java/com/hhh/gifapp/ui/mainFragment/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.hhh.gifapp.ui.mainFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hhh.gifapp.R
+import com.hhh.gifapp.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
